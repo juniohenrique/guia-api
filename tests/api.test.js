f56fc33121964269c25fdb97cd1fe333dd608db3
“Junio <juunio.henriquee@gmail.com>
2025-02-08 13:04:26 -0300

    remove poc test package for 500 error

--- tests/api.test.js
+++ tests/api.test.js
@@ -7,7 +7,7 @@ const mockRequest = jest.fn();
 
 describe('API Tests', () => {
 
-
+  describe('Mock fetch for success tests', () => {
   it('should return a list of users with status 200', async () => {
     const response = await request(baseURL).get('/users');
     expect(response.status).toBe(200);
@@ -27,4 +27,22 @@ describe('API Tests', () => {
     const response = await request(baseURL).get('');
     expect(response.status).toBe(500);
   });
+  require('jest-fetch-mock').enableMocks();
+
+  describe('Mock fetch for 500 error', () => {
+    beforeEach(() => {
+      fetch.mockResponseOnce(
+        JSON.stringify({ message: 'Internal Server Error' }),
+        { status: 500 }
+      );
+    });
+  
+    it('should mock 500 Internal Server Error with fetch', async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/1');
+      const body = await response.json();
+      expect(response.status).toBe(500);
+      expect(body.message).toBe('Internal Server Error');
+    });
+  });
+
 });
